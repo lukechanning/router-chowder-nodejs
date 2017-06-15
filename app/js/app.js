f97088a57b330dd51417e1d077aa67582b8c5ee3
Luke Patrick <luke@snodandjeff.com>
2017-06-15 21:10:52 +0000

    Jumbotron updates to match RC style

--- app/js/app.js
+++ app/js/app.js
@@ -1,20 +1,20 @@
-var angular = require('angular'); // angular
-//     _      _                         _    ___      _    _ _             _____ _           _ _          
-//   /_\  __| |_ ____ _ _ _  __ ___ __| |  / __|__ _| |__| (_)_ _  __ _  |_   _(_)_ __  ___| (_)_ _  ___ 
-//   / _ \/ _` \ V / _` | ' \/ _/ -_) _` | | (__/ _` | '_ \ | | ' \/ _` |   | | | | '  \/ -_) | | ' \/ -_)
-//  /_/ \_\__,_|\_/\__,_|_||_\__\___\__,_|  \___\__,_|_.__/_|_|_||_\__, |   |_| |_|_|_|_\___|_|_|_||_\___|
-//                                                                 |___/                                  
+// var angular = require('angular'); // angular
+// //     _      _                         _    ___      _    _ _             _____ _           _ _          
+// //   /_\  __| |_ ____ _ _ _  __ ___ __| |  / __|__ _| |__| (_)_ _  __ _  |_   _(_)_ __  ___| (_)_ _  ___ 
+// //   / _ \/ _` \ V / _` | ' \/ _/ -_) _` | | (__/ _` | '_ \ | | ' \/ _` |   | | | | '  \/ -_) | | ' \/ -_)
+// //  /_/ \_\__,_|\_/\__,_|_||_\__\___\__,_|  \___\__,_|_.__/_|_|_||_\__, |   |_| |_|_|_|_\___|_|_|_||_\___|
+// //                                                                 |___/                                  
  
-var app = angular.module('app', [
-        'ui.router'
-    ]);
+// var app = angular.module('app', [
+//         'ui.router'
+//     ]);
     
-require('angular-ui-router');
+// require('angular-ui-router');
     
-app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+// app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
     
-    $locationProvider.html5Mode(true).hashPrefix('!');
+//     $locationProvider.html5Mode(true).hashPrefix('!');
     
-    $urlRouterProvider.otherwise('/');
+//     $urlRouterProvider.otherwise('/');
     
-});
+// });
