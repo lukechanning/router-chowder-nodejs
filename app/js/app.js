f70b75d2660be76c7d827d5ce15905c4a0d7d2a8
Luke Patrick <luke@snodandjeff.com>
2017-06-19 17:37:34 +0000

    Refine work styling to include new images and add JS function to scroll them into view

--- app/js/app.js
+++ app/js/app.js
@@ -1,20 +1,30 @@
-// var angular = require('angular'); // angular
-// //     _      _                         _    ___      _    _ _             _____ _           _ _          
-// //   /_\  __| |_ ____ _ _ _  __ ___ __| |  / __|__ _| |__| (_)_ _  __ _  |_   _(_)_ __  ___| (_)_ _  ___ 
-// //   / _ \/ _` \ V / _` | ' \/ _/ -_) _` | | (__/ _` | '_ \ | | ' \/ _` |   | | | | '  \/ -_) | | ' \/ -_)
-// //  /_/ \_\__,_|\_/\__,_|_||_\__\___\__,_|  \___\__,_|_.__/_|_|_||_\__, |   |_| |_|_|_|_\___|_|_|_||_\___|
-// //                                                                 |___/                                  
- 
-// var app = angular.module('app', [
-//         'ui.router'
-//     ]);
+//   _____             _               _____ _                      _           
+//  |  __ \           | |             / ____| |                    | |          
+//  | |__) |___  _   _| |_ ___ _ __  | |    | |__   _____      ____| | ___ _ __ 
+//  |  _  // _ \| | | | __/ _ \ '__| | |    | '_ \ / _ \ \ /\ / / _` |/ _ \ '__|
+//  | | \ \ (_) | |_| | ||  __/ |    | |____| | | | (_) \ V  V / (_| |  __/ |   
+//  |_|  \_\___/ \__,_|\__\___|_|     \_____|_| |_|\___/ \_/\_/ \__,_|\___|_|   
+
+$(document).ready(function() {
     
-// require('angular-ui-router');
-    
-// app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-    
-//     $locationProvider.html5Mode(true).hashPrefix('!');
-    
-//     $urlRouterProvider.otherwise('/');
-    
-// });
+    // fire the scroll into view function
+    $(window).on("scroll resize", function() {
+        $(".faded").each(function(index, element) {
+            if (isScrolledIntoView(element)) {
+                $(element).animate({
+                    opacity: 1.0
+                }, 500);
+            }
+        });
+    });
+});
+
+function isScrolledIntoView(elem) {
+    var centerY = Math.max(0, (($(window).height() - $(elem).outerHeight()) - 50) +
+        $(window).scrollTop());
+
+    var elementTop = $(elem).offset().top;
+    var elementBottom = elementTop + $(elem).height();
+
+    return elementTop <= centerY && elementBottom >= centerY;
+}
--- public/app.js
+++ public/app.js
@@ -1,23 +1,32 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-// var angular = require('angular'); // angular
-// //     _      _                         _    ___      _    _ _             _____ _           _ _          
-// //   /_\  __| |_ ____ _ _ _  __ ___ __| |  / __|__ _| |__| (_)_ _  __ _  |_   _(_)_ __  ___| (_)_ _  ___ 
-// //   / _ \/ _` \ V / _` | ' \/ _/ -_) _` | | (__/ _` | '_ \ | | ' \/ _` |   | | | | '  \/ -_) | | ' \/ -_)
-// //  /_/ \_\__,_|\_/\__,_|_||_\__\___\__,_|  \___\__,_|_.__/_|_|_||_\__, |   |_| |_|_|_|_\___|_|_|_||_\___|
-// //                                                                 |___/                                  
- 
-// var app = angular.module('app', [
-//         'ui.router'
-//     ]);
-    
-// require('angular-ui-router');
-    
-// app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-    
-//     $locationProvider.html5Mode(true).hashPrefix('!');
-    
-//     $urlRouterProvider.otherwise('/');
-    
-// });
+//   _____             _               _____ _                      _           
+//  |  __ \           | |             / ____| |                    | |          
+//  | |__) |___  _   _| |_ ___ _ __  | |    | |__   _____      ____| | ___ _ __ 
+//  |  _  // _ \| | | | __/ _ \ '__| | |    | '_ \ / _ \ \ /\ / / _` |/ _ \ '__|
+//  | | \ \ (_) | |_| | ||  __/ |    | |____| | | | (_) \ V  V / (_| |  __/ |   
+//  |_|  \_\___/ \__,_|\__\___|_|     \_____|_| |_|\___/ \_/\_/ \__,_|\___|_|   
 
+$(document).ready(function() {
+    
+    // fire the scroll into view function
+    $(window).on("scroll resize", function() {
+        $(".faded").each(function(index, element) {
+            if (isScrolledIntoView(element)) {
+                $(element).animate({
+                    opacity: 1.0
+                }, 500);
+            }
+        });
+    });
+});
+
+function isScrolledIntoView(elem) {
+    var centerY = Math.max(0, (($(window).height() - $(elem).outerHeight()) - 50) +
+        $(window).scrollTop());
+
+    var elementTop = $(elem).offset().top;
+    var elementBottom = elementTop + $(elem).height();
+
+    return elementTop <= centerY && elementBottom >= centerY;
+}
 },{}]},{},[1]);
